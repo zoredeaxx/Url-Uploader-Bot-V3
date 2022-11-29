be6a17baa1420b2d700c256bb52cff725a2ccc5e
zoredeaxx <87483466+zoredeaxx@users.noreply.github.com>
2022-11-29 15:53:45 +0530

    Update display_progress.py

--- Uploader/functions/display_progress.py
+++ Uploader/functions/display_progress.py
@@ -4,19 +4,10 @@ import time
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from Uploader.script import Translation
 
-
-
-async def progress_for_pyrogram(
-    current,
-    total,
-    ud_type,
-    message,
-    start
-):
+async def progress_for_pyrogram(current, total, ud_type, message, start):
     now = time.time()
     diff = now - start
     if round(diff % 10.00) == 0 or current == total:
-        # if round(current / total * 100, 0) % 5 == 0:
         percentage = current * 100 / total
         speed = current / diff
         elapsed_time = round(diff) * 1000
@@ -26,24 +17,74 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \nP: {2}%\n".format(
-            ''.join(["◾" for _ in range(math.floor(percentage / 5))]),
-            ''.join(["◽" for _ in range(20 - math.floor(percentage / 5))]),
-            round(percentage, 2),
+        progress = "[{0}{1}] \n".format(
+            ''.join(["█" for i in range(math.floor(percentage / 5))]),
+            ''.join(["" for i in range(20 - math.floor(percentage / 5))])
         )
 
-
-        tmp = progress + "{0} of {1}\n\nSpeed: {2}/s\n\nETA: {3}\n\n".format(
+        tmp = progress + Translation.PROGRESS.format(
+            round(percentage, 2),
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
-            # elapsed_time if elapsed_time != '' else "0 s",
             estimated_total_time if estimated_total_time != '' else "0 s"
         )
         try:
-            await message.edit(text=f"{ud_type}\n {tmp}")
+            await message.edit(
+              text="**{}**\n\n {}".format(
+              ud_type,
+              tmp
+                ),
+                parse_mode='markdown',
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [ 
+                        InlineKeyboardButton('♨️ Cancel', callback_data='close')
+                       ]
+                   ]
+                 )
+            )
         except:
             pass
+
+#async def progress_for_pyrogram(
+    #current,
+    #total,
+    #ud_type,
+    #message,
+    #start
+#):
+    #now = time.time()
+    #diff = now - start
+    #if round(diff % 10.00) == 0 or current == total:
+        ## if round(current / total * 100, 0) % 5 == 0:
+        #percentage = current * 100 / total
+        #speed = current / diff
+        #elapsed_time = round(diff) * 1000
+        #time_to_completion = round((total - current) / speed) * 1000
+        #estimated_total_time = elapsed_time + time_to_completion
+
+        #elapsed_time = TimeFormatter(milliseconds=elapsed_time)
+        #estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
+
+        #progress = "[{0}{1}] \nP: {2}%\n".format(
+            #''.join(["◾" for _ in range(math.floor(percentage / 5))]),
+            #''.join(["◽" for _ in range(20 - math.floor(percentage / 5))]),
+            #round(percentage, 2),
+        #)
+
+
+        #tmp = progress + "{0} of {1}\n\nSpeed: {2}/s\n\nETA: {3}\n\n".format(
+            #humanbytes(current),
+            #humanbytes(total),
+            #humanbytes(speed),
+            ## elapsed_time if elapsed_time != '' else "0 s",
+            #estimated_total_time if estimated_total_time != '' else "0 s"
+        #)
+        #try:
+            #await message.edit(text=f"{ud_type}\n {tmp}")
+        #except:
+            #pass
 
 
 
