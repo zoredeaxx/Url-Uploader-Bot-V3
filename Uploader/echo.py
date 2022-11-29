ed28cdb4bee472fde20a903fbb70f69a9a59bc0c
zoredeaxx <87483466+zoredeaxx@users.noreply.github.com>
2022-11-29 17:53:04 +0530

    Update echo.py

--- Uploader/echo.py
+++ Uploader/echo.py
@@ -38,21 +38,21 @@ f = filters.private & filters.regex(pattern=".*http.*")
 @Client.on_message(f)
 
 async def echo(bot, update):
-    #if Config.LOG_CHANNEL:
-        #try:
-            #log_message = await update.forward(Config.LOG_CHANNEL)
-            #log_info = "Message Sender Information\n"
-            #log_info += "\nFirst Name: " + update.from_user.first_name
-            #log_info += "\nUser ID: " + str(update.from_user.id)
-            #log_info += "\nUsername: @" + update.from_user.username if update.from_user.username else ""
-            #log_info += "\nUser Link: " + update.from_user.mention
-            #await log_message.reply_text(
-                #text=log_info,
-                #disable_web_page_preview=True,
-                #quote=True
-            #)
-        #except Exception as error:
-            #print(error)
+    if Config.LOG_CHANNEL:
+        try:
+            log_message = await update.forward(Config.LOG_CHANNEL)
+            log_info = "Message Sender Information\n"
+            log_info += "\nFirst Name: " + update.from_user.first_name
+            log_info += "\nUser ID: " + str(update.from_user.id)
+            log_info += "\nUsername: @" + update.from_user.username if update.from_user.username else ""
+            log_info += "\nUser Link: " + update.from_user.mention
+            await log_message.reply_text(
+                text=log_info,
+                disable_web_page_preview=True,
+                quote=True
+            )
+        except Exception as error:
+            print(error)
     if not update.from_user:
         return await update.reply_text("I don't know about you sir :(")
     await AddUser(bot, update)
